cb6d780486fc4dcf88f415997fe09eb6de42f8f6
RKevo <64652049+RKevo@users.noreply.github.com>
2025-10-01 16:18:08 +0700

    remove redundant import

--- Assets/JANOARG.Chartmaker/Scripts/UI/Modal/ModalTypes/RenderModal.cs
+++ Assets/JANOARG.Chartmaker/Scripts/UI/Modal/ModalTypes/RenderModal.cs
@@ -1,12 +1,10 @@
 using System;
-using System.Buffers;
 using System.Collections;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
