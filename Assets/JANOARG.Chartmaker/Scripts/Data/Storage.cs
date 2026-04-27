b16fb228434b21adc4a9228ff76bc1ee7c8be53f
ducdat0507 <62660527+ducdat0507@users.noreply.github.com>
2026-04-27 15:44:02 +0700

    Fix recent song list getting clear on app start

--- Assets/JANOARG.Chartmaker/Scripts/Data/Storage.cs
+++ Assets/JANOARG.Chartmaker/Scripts/Data/Storage.cs
@@ -1,10 +1,12 @@
 using System.Xml.Serialization;
 using JANOARG.Shared.Data.ChartInfo;
+using JANOARG.Shared.Data.Chartmaker;
 
 namespace JANOARG.Chartmaker.Data
 {
 
     [XmlRoot("ItemList")]
+    [XmlInclude(typeof(RecentSong))]
     public class ClientSerializeProxyList : SerializeProxyList
     {
     }
